2347a96e1a06b3263a502e3da27ae866b0e8cff2
ciamara <mruktamara64@gmail.com>
2026-03-14 09:05:31 +0100

    small refactor

--- angular-frontend/src/app/app.spec.ts
+++ angular-frontend/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lab5');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-frontend');
   });
 });
